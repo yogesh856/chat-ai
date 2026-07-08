43e693574c77d5bdcc1c2b7ea913c45b7d59776c
yogesh856 <ghuleyogesh856@gmail.com>
2026-07-08 13:24:14 +0530

    Intial Commit

---
+++ src/main/java/chat_ai/FirstAiProjectApplication.java
@@ -0,0 +1,13 @@
+package chat_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAiProjectApplication.class, args);
+	}
+
+}
